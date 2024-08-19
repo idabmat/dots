b11eda9d3d1f3c65ac6627b282a66c6dc5f90974
Igor de Alcantara Barroso <igor@talimhq.com>
2024-08-19 17:27:28 +0100

    Server config

--- git.nix
+++ git.nix
@@ -5,10 +5,6 @@
     enable = true;
     userName = "Igor de Alcântara Barroso";
     userEmail = "igor@talimhq.com";
-    signing = {
-      key = "7D1685987BD2D2FEC9C5D95E794A41BA3DBE9931";
-      signByDefault = true;
-    };
     delta = {
       enable = true;
       options = {
@@ -38,13 +34,13 @@
     };
   };
 
-  programs.gpg = {
-    enable = true;
-  };
+  # programs.gpg = {
+  #   enable = true;
+  # };
+  #
+  # services.gpg-agent = {
+  #   enable = true;
+  #   enableSshSupport = true;
+  #   pinentryPackage = pkgs.pinentry-tty;
+  # };
 
-  services.gpg-agent = {
-    enable = true;
-    enableSshSupport = true;
-    pinentryPackage = pkgs.pinentry-tty;
-  };
-}
