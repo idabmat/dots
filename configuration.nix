dce3d0f64f99d3d2ca2c2ababb3d087c94ce1b19
Igor de Alcantara Barroso <igor@talimhq.com>
2025-02-06 12:13:14 +0100

    Copy configuration

--- configuration.nix
+++ configuration.nix
@@ -4,19 +4,6 @@
 
 { config, lib, pkgs, ... }:
 
-let
-  zfsCompatibleKernelPackages = lib.filterAttrs (
-    name: kernelPackages:
-    (builtins.match "linux_[0-9]+_[0-9]+" name) != null
-    && (builtins.tryEval kernelPackages).success
-    && (!kernelPackages.${config.boot.zfs.package.kernelModuleAttribute}.meta.broken)
-  ) pkgs.linuxKernel.packages;
-  latestKernelPackage = lib.last (
-    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
-      builtins.attrValues zfsCompatibleKernelPackages
-    )
-  );
-in
 {
   nixpkgs = {
     config = {
@@ -24,7 +11,7 @@ in
       cudaSupport = true;
     };
   };
-  
+
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
@@ -32,38 +19,40 @@ in
 
   # Use the systemd-boot EFI boot loader.
   boot = {
-    kernelPackages = latestKernelPackage;
     loader = {
-      systemd-boot = {
-        enable = true;
-      };
       efi = {
         canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot/efi";
+      };
+      grub = {
+        enable = true;
+        device = "nodev";
+        efiSupport = true;
+        enableCryptodisk = true;
       };
     };
     initrd = {
-      kernelModules = ["nvidia"];
+      luks = {
+        devices = {
+          cryptlvm = {
+            device = "/dev/disk/by-uuid/146caf35-05b1-4bb4-93af-34e0af81399a";
+            preLVM = true;
+            allowDiscards = true;
+            keyFile = "/keyfile";
+          };
+	};
+      };
+      secrets = {
+        "keyfile" = "/etc/secrets/keyfile";
+      };
+      kernelModules = [ "nvidia" ];
     };
-    supportedFilesystems = ["zfs"];
-    zfs = {
-      forceImportRoot = false;
-      extraPools = ["data"];
-    };
-  };
-
-  nix = {
-    package = pkgs.nixVersions.stable;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-    settings = {
-      substituters = [
-        "https://cuda-maintainers.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
-    };
+    kernelParams = [
+      "amd_pstate=active"
+      "amdgpu.sg_display=0"
+      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+    ];
+    kernelPackages = pkgs.linuxPackages_latest;
   };
 
   hardware = {
@@ -77,6 +66,14 @@ in
     };
     nvidia = {
       package = config.boot.kernelPackages.nvidiaPackages.beta;
+      prime = {
+        amdgpuBusId = "PCI:9:0:0";
+        nvidiaBusId = "PCI:1:0:0";
+        offload = {
+          enable = true;
+          enableOffloadCmd = true;
+        };
+      };
       modesetting = {
         enable = true;
       };
@@ -96,14 +93,8 @@ in
         enable = true;
       };
     };
-    pulseaudio = {
-      enable = false;
-    };
   };
 
-  # networking.hostName = "nixos"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking = {
     networkmanager = {
       enable = true;  # Easiest to use and most distros use this by default.
@@ -111,143 +102,57 @@ in
     firewall = {
       checkReversePath = "loose";
     };
-    hostId = "b756db4b";
   };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Lisbon";
+  time.timeZone = "Africa/Niamey";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # hardware.pulseaudio.enable = true;
-  # OR
 
   security = {
     rtkit = {
       enable = true;
     };
   };
-  
-  services = {
-    tailscale = {
+
+  virtualisation = {
+    docker = {
       enable = true;
     };
-    pipewire = {
-      enable = true;
-      pulse = {
-        enable = true;
-      };
-    };
-    greetd = {
-      enable = true;
-      settings = {
-        default_session = {
-          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-          user = "greeter";
-        };
-      };
-    };
-    pcscd = {
-      enable = true;
-    };
-    xserver = {
-      videoDrivers = ["nvidia"];
-    };
-    blueman = {
-      enable = true;
-    };
-    hardware = {
-      openrgb = {
-        enable = true;
-      };
-    };
-    ollama = {
-      enable = true;
-      acceleration = "cuda";
-    };
-    gvfs = {
-      enable = true;
-    };
-    openssh = {
-      enable = true;
-      ports = [2222];
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        AllowUsers = ["me"];
-        PermitRootLogin = "no";
-      };
-    };
-    zfs = {
-      autoScrub = {
-        enable = true;
+  };
+
+  users = {
+    users = {
+      me = {
+        isNormalUser = true;
+        shell = pkgs.zsh;
+        extraGroups = [ "wheel" "docker" ];
       };
     };
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.me = {
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    openssh = {
-      authorizedKeys = {
-        keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPg+ljxM+kwwIyvSwIDf5oTDBgU5zA168oAcu4oo3KjW me"
-        ];
-      };
-    };
-  #   packages = with pkgs; [
-  #     firefox
-  #     tree
-  #   ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment = {
     systemPackages = with pkgs; [
-      helix # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-      ntfs3g
+      helix
     ];
     shells = with pkgs; [
       zsh
     ];
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  nix = {
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = [
+        "https://cuda-maintainers.cachix.org"
+      ];
+      trusted-public-keys = [
+        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      ];
+    };
+  };
+
   programs = {
-    hyprland = {
+    hyprland =  {
       enable = true;
     };
     zsh = {
@@ -264,40 +169,67 @@ in
     };
   };
 
-  # List services that you want to enable:
+  services = {
+    tailscale = {
+      enable = true;
+      useRoutingFeatures = "client";
+    };
+    pulseaudio = {
+      enable = false;
+    };
+    pipewire = {
+      enable = true;
+      pulse = {
+        enable = true;
+      };
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+    };
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+          user = "greeter";
+        };
+      };
+    };
+    pcscd = {
+      enable = true;
+    };
+    udev = {
+      packages = [pkgs.yubikey-personalization];
+    };
+    xserver = {
+      videoDrivers = ["nvidia"];
+    };
+    blueman = {
+      enable = true;
+    };
+    ollama = {
+      enable = true;
+      acceleration = "cuda";
+    };
+    gvfs = {
+      enable = true;
+    };
+    upower = {
+      enable = true;
+    };
+    power-profiles-daemon = {
+      enable = true;
+    };
+    supergfxd = {
+      enable = true;
+    };
+    asusd = {
+      enable = true;
+      enableUserService = true;
+    };
+  };
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "24.05"; # Did you read the comment?
-
+  system.stateVersion = "23.11";
 }
 
