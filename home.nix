d543937ca6fc96db692cb52ce53729ddb7d8a428
Igor de Alcantara Barroso <igor@talimhq.com>
2025-05-09 22:45:32 +0100

    Add gsync env vars

--- home.nix
+++ home.nix
@@ -568,6 +568,8 @@ in
             "LIBVA_DRIVER_NAME,nvidia"
             "GBM_BACKEND,nvidia-drm"
             "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+            "__GL_GSYNC_ALLOWED,1"
+            "__GL_VRR_ALLOWED,1"
             "NVD_BACKEND,direct"
             "XCURSOR_SIZE,32"
             "HYPRCURSOR_THEME,BreezeX-RosePine-Linux"
