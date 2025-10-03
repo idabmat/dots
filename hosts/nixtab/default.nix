4cdf51876e2d9b622f0d6a3bd69e2859b129d3b6
Igor de Alcantara Barroso <igor@talimhq.com>
2025-10-03 10:56:01 +0100

    Update flake, remove amdvlk

--- hosts/nixtab/default.nix
+++ hosts/nixtab/default.nix
@@ -75,10 +75,6 @@
       enable32Bit = true;
       extraPackages = with pkgs; [
         rocmPackages.clr.icd
-        amdvlk
-      ];
-      extraPackages32 = with pkgs; [
-        driversi686Linux.amdvlk
       ];
     };
     bluetooth = {
@@ -101,14 +97,6 @@
     };
     i2c = {
       enable = true;
-    };
-    amdgpu = {
-      amdvlk = {
-        enable = true;
-        support32Bit = {
-          enable = true;
-        };
-      };
     };
   };
 
