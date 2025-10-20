c2369a44b0f7a4ed5f56cd6f551fafd15067f8d8
Igor de Alcantara Barroso <igor@talimhq.com>
2025-10-20 19:48:20 +0100

    Update flake

--- users/me/home.nix
+++ users/me/home.nix
@@ -144,38 +144,41 @@ in
       };
     };
     git = {
+      settings = {
+        user = {
+
+          email = "igor@talimhq.com";
+          name = "Igor de Alcantara Barroso";
+          github = {
+            user = "idabmat";
+          };
+          pull = {
+            rebase = false;
+          };
+          merge = {
+            confictstyle = "diff3";
+          };
+          init = {
+            defaultBranch = "main";
+          };
+          pager = {
+            diff = "delta";
+            log = "delta";
+            reflog = "delta";
+            show = "delta";
+          };
+        };
+      };
       enable = true;
-      userName = "Igor de Alcantara Barroso";
-      userEmail = "igor@talimhq.com";
       signing = {
         key = "794A41BA3DBE9931";
         signByDefault = true;
       };
-      delta = {
-        enable = true;
-        options = {
-          side-by-side = true;
-        };
-      };
-      extraConfig = {
-        github = {
-          user = "idabmat";
-        };
-        pull = {
-          rebase = false;
-        };
-        merge = {
-          confictstyle = "diff3";
-        };
-        init = {
-          defaultBranch = "main";
-        };
-        pager = {
-          diff = "delta";
-          log = "delta";
-          reflog = "delta";
-          show = "delta";
-        };
+    };
+    delta = {
+      enable = true;
+      options = {
+        side-by-side = true;
       };
     };
 
