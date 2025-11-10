0e46e4aed7ecfc4cf1cbd06556d947cea0f3e4b3
Igor de Alcantara Barroso <igor@talimhq.com>
2025-11-10 19:07:35 +0000

    Switch back to flake packaged expert-ls

--- users/me/nvim/lua/plugins/lsp.lua
+++ users/me/nvim/lua/plugins/lsp.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.lsp.config('expert', {
-  cmd = { '/home/me/code/expert/apps/expert/burrito_out/expert_linux_amd64', '--stdio' },
+  cmd = { 'expert', '--stdio' },
   root_markers = { 'mix.exs', '.git' },
   filetypes = { 'elixir', 'eelixir', 'heex', 'surface' }
 })
