28e9836ce00730fdf36ed506fbbc545fcc09ee90
Igor de Alcantara Barroso <igor@talimhq.com>
2026-04-06 20:34:26 +0100

    Update flake

--- users/me/home.nix
+++ users/me/home.nix
@@ -372,6 +372,8 @@ in {
       defaultEditor = true;
       viAlias = true;
       vimAlias = true;
+      withRuby = false;
+      withPython3 = false;
       plugins = with pkgs.vimPlugins; [
         codecompanion-nvim
         cmp-buffer
@@ -476,7 +478,7 @@ in {
     opencode = {
       enable = true;
       enableMcpIntegration = true;
-      settings = {
+      tui = {
         theme = "dracula";
       };
     };
