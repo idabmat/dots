8ac9f4a5385a4a62bdab16f1488cea2c811c4c37
Igor de Alcantara Barroso <igor@talimhq.com>
2026-07-14 16:44:35 +0100

    Use rootless docker

--- hosts/nixtab/default.nix
+++ hosts/nixtab/default.nix
@@ -277,13 +277,17 @@
   };
 
   virtualisation.docker = {
-    enable = true;
-    daemon = {
-      settings = {
-        dns = [
-          "1.1.1.1"
-          "8.8.8.8"
-        ];
+    enable = false;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+      daemon = {
+        settings = {
+          dns = [
+            "1.1.1.1"
+            "8.8.8.8"
+          ];
+        };
       };
     };
   };
